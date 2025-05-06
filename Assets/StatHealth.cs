1d0ee2fd22a498fd50aad03adb1188baf27c10b4
Marcin Pawłowski <marcin.pawlowski1@microsoft.wsei.edu.pl>
2025-05-06 03:20:33 +0200

    Implementing Health Regen statistic

--- Assets/StatHealth.cs
+++ Assets/StatHealth.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class StatHealth : MonoBehaviour
 {
-    public int Health;
+    public int Max_Health;
+    private int Health;
+
     public int Healing_From_Damage_Percent;
+
+    public int Health_Regen;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(RegenHealth());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Max_Health == Health)
+        {
+            StopCoroutine(RegenHealth());
+        }
     }
 
     public int GetHealth()
@@ -26,5 +33,14 @@ public class StatHealth : MonoBehaviour
     public void HealFromDamage(int damage)
     {
         Health = damage * Healing_From_Damage_Percent;
+    }
+
+    IEnumerator RegenHealth()
+    {
+        while (true)
+        {
+            Health += Health_Regen;
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
