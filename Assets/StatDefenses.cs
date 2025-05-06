d999cc080fe4cfff4ffebcb0ff45e048f040f8d4
Marcin Pawłowski <marcin.pawlowski1@microsoft.wsei.edu.pl>
2025-05-06 02:33:33 +0200

    Implementing Armor statistic

---
+++ Assets/StatDefenses.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatDefenses : MonoBehaviour
+{
+    public int Armor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetDamageByDamageType(int damage, int damageType)
+    {
+        if (damageType == "Physical")
+        {
+            return damage - Armor;
+        }
+        else return damage;
+    }
+
+    public void GetArmor()
+    {
+        return Armor;
+    }
+}
