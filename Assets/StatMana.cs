685ab63ff72784ac3c98963ef9b174bdd5adfecf
marcinoPlayGames <marcin.pawlowski410@gmail.com>
2025-05-08 02:41:32 +0200

    Implementing Mana statistic

---
+++ Assets/StatMana.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatMana : MonoBehaviour
+{
+    public int Max_Mana;
+    private int Mana;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetMana()
+    {
+        return Mana;
+    }
+}
