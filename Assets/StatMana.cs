a8574dd69bb30e0d937529247ad39c3c9ec1eb3f
marcinoPlayGames <marcin.pawlowski410@gmail.com>
2025-05-08 02:43:06 +0200

    Implementing Mana Regen statistic

--- Assets/StatMana.cs
+++ Assets/StatMana.cs
@@ -6,20 +6,34 @@ public class StatMana : MonoBehaviour
 {
     public int Max_Mana;
     private int Mana;
+
+    public int Mana_Regen;
     // Start is called before the first frame update
     void Start()
     {
-
+        StartCoroutine(RegenMana());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Max_Mana == Mana)
+        {
+            StopCoroutine(RegenMana());
+        }
     }
 
     public int GetMana()
     {
         return Mana;
+    }
+
+    IEnumerator RegenMana()
+    {
+        while (true)
+        {
+            Mana += Mana_Regen;
+            yield return new WaitForSeconds(1f);
+        }
     }
 }
