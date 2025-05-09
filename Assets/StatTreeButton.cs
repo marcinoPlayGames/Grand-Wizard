f3718847ace94437edddf801fc5920183c6fdb69
marcinoPlayGames <marcin.pawlowski410@gmail.com>
2025-05-09 03:31:33 +0200

    Creating UI panel for buying system with coins and treasures

---
+++ Assets/StatTreeButton.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatTreeButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string statId = Attack_Damage;
+
+    public void OnClick()
+    {
+        StatUIController.Instance.ShowUpgradeWindow(statId);
+    }
+}
