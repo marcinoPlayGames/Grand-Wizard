255e258dc1fd1079a3da3597c9beb6bc2dd4ae62
marcinoPlayGames <marcin.pawlowski410@gmail.com>
2025-05-22 23:25:18 +0200

    Making Buttons activating system

--- Assets/MovingObject.cs
+++ Assets/MovingObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingSpikes : MonoBehaviour
+public class MovingObject : MonoBehaviour
 {
     public int trapMovingXDistance = -15;
     public int trapMovingYDistance = 0;
--- Assets/TriggerDoor.cs
+++ Assets/TriggerDoor.cs
@@ -1,24 +1,28 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerSpikes : MonoBehaviour
+public class TriggerDoor : MonoBehaviour
 {
     public GameObject triggeredObject;
 
     private MovingObject movingObject;
+
+    private MovingObject movingButton;
 
     // Start is called before the first frame update
     void Start()
     {
         if (triggeredObject != null)
         {
-            // Getting component of MovingObject from TriggeredObject
+            // Getting component of MovingSpikes from TriggeredObject
             movingObject = triggeredObject.GetComponent<MovingObject>();
+
+            movingButton = this.GetComponent<MovingObject>();
 
             if (movingObject == null)
             {
-                Debug.LogWarning("TriggeredObject don't have component MovingObject!");
+                Debug.LogWarning("TriggeredObject don't have component MovingDoor!");
             }
         }
     }
@@ -29,11 +33,12 @@ public class TriggerSpikes : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        if (movingObject != null && other.CompareTag("Player"))
+        if (movingObject != null && other.gameObject.CompareTag("Player"))
         {
             movingObject.Activate();
+            movingButton.Activate();
         }
     }
 }
