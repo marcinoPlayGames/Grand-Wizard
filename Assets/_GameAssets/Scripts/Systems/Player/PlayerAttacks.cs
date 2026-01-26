e9d336973d4ed27e4a3b2748feb3ea0c6b5a39bf
marcinoPlayGames <marcin.pawlowski1@microsoft.wsei.edu.pl>
2026-01-26 20:01:50 +0100

    Fixing Enemies collisions and player attacks

--- Assets/_GameAssets/Scripts/Systems/Player/PlayerAttacks.cs
+++ Assets/_GameAssets/Scripts/Systems/Player/PlayerAttacks.cs
@@ -252,10 +252,15 @@ public class PlayerAttacks : MonoBehaviour
 
             if (controller != null)
             {
+                Debug.Log($"spawnPos: {spawnPosition}");
+                Debug.Log($"range: {attackRange}");
+
                 controller.SetDamage(totalDamage);
                 controller.SetTracking(spawnPosition, attackRange);
             }
         }
+
+        Debug.Log("Fireball spawned!");
 
         StartCoroutine(FireballCooldown());
     }
